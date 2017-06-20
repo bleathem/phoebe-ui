5b70556f1196b39bce8abf78e6fbe4c80f9f27d3
Brian Leathem <bleathem@gmail.com>
2017-06-20 09:47:04 -0700

    Toggled the display of the test-case-status-by-pipeline-run details table

--- src/app/test-case-status-by-pipeline-run/test-case-status-by-pipeline-run.component.ts
+++ src/app/test-case-status-by-pipeline-run/test-case-status-by-pipeline-run.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import * as c3 from 'c3';
 
 @Component({
@@ -7,6 +7,7 @@ import * as c3 from 'c3';
   styleUrls: ['./test-case-status-by-pipeline-run.component.less']
 })
 export class TestCaseStatusByPipelineRunComponent implements OnInit {
+  @Input() hideDetails: boolean = true;
 
   constructor() { }
 
