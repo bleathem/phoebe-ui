9753264f27e17d45be5d2c3d5134580c1a5da9f2
Brian Leathem <bleathem@gmail.com>
2017-07-17 19:19:43 -0700

    Re-factored the dataLoaded zip logic to the parent component

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,13 +1,5 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, Output, OnChanges, OnInit, SimpleChanges, EventEmitter } from '@angular/core';
 import * as c3 from 'c3';
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/observable/zip';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/buffer';
-import 'rxjs/add/operator/bufferCount';
-import 'rxjs/add/operator/skipUntil';
 
 @Component({
   selector: 'charts-line-chart',
@@ -16,14 +8,10 @@ import 'rxjs/add/operator/skipUntil';
 })
 export class LineChartComponent implements OnInit, OnChanges {
   @Input() data: (string | number)[][];
-  @Input() dataStream: Observable<(string | number)[][]>;
+  @Output() dataLoaded: EventEmitter<any> = new EventEmitter();
   chart: any;
-  subscription: Subscription;
-  dataEvents: Subject<(string | number)[][]>;
-  doneEvents: Subject<any>;
 
-  constructor(private el: ElementRef) {
-  }
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
     var lineChartDataColumns = [];
@@ -47,22 +35,13 @@ export class LineChartComponent implements OnInit, OnChanges {
     };
     lineChartConfig.data.columns = this.data || [];
     this.chart = c3.generate(lineChartConfig);
-    this.dataEvents = new Subject();
-    this.doneEvents = new Subject();
-    Observable.zip(
-      this.dataEvents,
-      this.doneEvents,
-      (data, done) => data
-    )
-    .subscribe(data => {
-      this.chart.load({ columns: data, unload: true, done: () => {this.doneEvents.next()} });
-    });
-    this.doneEvents.next();
+
+    this.dataLoaded.next();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.chart && changes.data) {
-      this.dataEvents.next(changes.data.currentValue);
+      this.chart.load({ columns: changes.data.currentValue, unload: true, done: () => {this.dataLoaded.next()} });
     }
   }
 
--- src/app/reports/test-case-status-by-pipeline-run/test-case-status-by-pipeline-run.component.ts
+++ src/app/reports/test-case-status-by-pipeline-run/test-case-status-by-pipeline-run.component.ts
@@ -4,8 +4,10 @@ import { Store } from '@ngrx/store';
 import { AppStore } from '../../app.store';
 import { PackageBuild, TestCase, TestSuite } from '../../pipeline/pipeline.model';
 
+import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/zip';
 
 
 @Component({
@@ -16,15 +18,28 @@ import 'rxjs/add/operator/filter';
 export class TestCaseStatusByPipelineRunComponent implements OnInit {
   @Input() hideDetails: boolean = true;
   @Input() chartData: Observable<PackageBuild[]>;
+  dataLoaded: Subject<any> = new Subject();
 
   constructor(private testCaseStatusService: TestCaseStatusService, private store: Store<AppStore>) {
-    // this.chartData = this.testCaseStatusService.byPipelineRunObservable.map(data => [['x', 246, 247, 248, 250, 251, 252]].concat(data));
-    this.chartData = store
+    let chartData = store
     .select(store => store.pipelineReducer.selectedPipeline && store.pipelineReducer.selectedPipeline.packageBuilds)
     .filter(state => !!state);
+
+    this.chartData = Observable.zip(
+      chartData,
+      this.dataLoaded,
+      (data, done) => data
+    );
+  }
+
+  onDataLoaded() {
+    setTimeout(() => {
+      this.dataLoaded.next();
+    })
   }
 
   ngOnInit() {
+    this.dataLoaded.next();
   }
 
 }
